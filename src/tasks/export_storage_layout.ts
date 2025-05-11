d485920b404a4331ea5c332f4ae3c91cfe456c09
Nick Barry <itsnickbarry@protonmail.ch>
2025-05-11 11:26:41 -0600

    set export task description

--- src/tasks/export_storage_layout.ts
+++ src/tasks/export_storage_layout.ts
@@ -1,8 +1,8 @@
 import { TASK_EXPORT_STORAGE_LAYOUT } from '../task_names.js';
 import { task } from 'hardhat/config';
 
-// TODO: set description
 export default task(TASK_EXPORT_STORAGE_LAYOUT)
+  .setDescription('Export storage layouts to files')
   .addFlag({
     name: 'noCompile',
     description: "Don't compile before running this task",
