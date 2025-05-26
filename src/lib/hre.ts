dcd6e3b7552796df98a7b3277e524f9eb9920a67
Nick Barry <itsnickbarry@protonmail.ch>
2025-05-26 15:59:28 -0600

    pass config hook to git hre

--- src/lib/hre.ts
+++ src/lib/hre.ts
@@ -1,13 +1,24 @@
+import pkg from '../../package.json';
 import { createHardhatRuntimeEnvironmentAtGitRef } from '@solidstate/hardhat-git';
 import type { HardhatConfig } from 'hardhat/types/config';
+import type { HardhatPlugin } from 'hardhat/types/plugins';
 
 export const prepareHardhatRuntimeEnvironment = async (
   originConfig: HardhatConfig,
   ref?: string,
 ) => {
-  // TODO: initialize hre with plugin or user config containing storageLayout output selection
+  // pass in config hooks to ensure that storage layout is inluded in solc
+  // output selection even if this plugin was not present at git ref
+  const plugin: HardhatPlugin = {
+    id: `${pkg.name}_temp`,
+    hookHandlers: {
+      config: import.meta.resolve('../hooks/config.js'),
+    },
+  };
 
-  const hre = await createHardhatRuntimeEnvironmentAtGitRef(originConfig, ref);
+  const hre = await createHardhatRuntimeEnvironmentAtGitRef(originConfig, ref, [
+    plugin,
+  ]);
 
   return hre;
 };
